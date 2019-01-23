351d126f762e5c6f81904f466debc7f805b18852
Xavier <xavier.touikan@gmail.com>
2019-01-23 12:03:02 -0500

    test1(PinballGame)

---
+++ test1/Assets/LaunchScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string buttons = "Launch";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetButton(buttons)){
+            this.GetComponent<ConstantForce> ().enabled = true;
+        }
+        else {
+            this.GetComponent<ConstantForce> ().enabled = false;
+        }
+        
+    }
+}
---
+++ test1/Assets/PaddlesBehav.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddlesBehav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string buttons = "LeftFlipper";
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetButton(buttons)){
+            this.GetComponent<HingeJoint> ().useMotor = true;
+        }
+        else {
+            this.GetComponent<HingeJoint> ().useMotor = false;
+        }
+    }   
+}
